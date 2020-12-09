82241e429bdb377a456f3a9c7c44c312652d2a03
ax14m <telco.alexander@gmail.com>
2020-12-09 12:30:54 -0500

    9122020
    9122020 api_tienda

---
+++ Modelos/TiendaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TiendaApi.Modelos
+{
+    
+        public class TiendaContext : DbContext
+        {
+            public TiendaContext (DbContextOptions<TiendaContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TiendaItem> TiendaItems { get; set; }
+        }
+    }
+
